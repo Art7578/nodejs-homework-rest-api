796d4626ea9935a0133ca4b80fd7eb8906a717aa
Art7578 <113849803+Art7578@users.noreply.github.com>
2023-07-31 15:59:26 +0300

    hw_04

---
+++ schemas/user-schema.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+const userRegisterSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+});
+
+const userLoginSchema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+});
+
+export default {
+    userRegisterSchema,
+    userLoginSchema,
+};
